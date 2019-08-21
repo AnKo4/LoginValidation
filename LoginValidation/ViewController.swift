ab63345a157daba7aa455a1cdd28c63c49738b7b
Andrey <andrey@Andreys-Mac.local>
2019-08-21 14:53:26 +0400

    Initial Commit

---
+++ LoginValidation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginValidation
+//
+//  Created by Andrey on 21/08/2019.
+//  Copyright © 2019 Andrey Kochetkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
